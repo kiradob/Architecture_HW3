8f7e594b1e60cf5a01402e52697d98a89ad211a3
kira.dob <arik.dobrynina@yandex.ru>
2023-10-07 00:26:29 +0700

    added Homework3 Interfaces

---
+++ Interfaces/ICarWash.java
@@ -0,0 +1,9 @@
+package Architecture_HW3.Interfaces;
+
+public class ICarWash {
+    public void wipWindshield();
+
+    public void wipHeadlights();
+
+    public void wipMirrors();
+}
---
+++ Interfaces/IFuelStation.java
@@ -0,0 +1,12 @@
+package Architecture_HW3.Interfaces;
+
+public interface IFuelStation {
+    
+    public void fuel();
+
+    public void wipWindshield();
+
+    public void wipHeadlights();
+
+    public void wipMirrors();
+}
